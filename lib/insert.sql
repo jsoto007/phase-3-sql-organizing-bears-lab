39b48d47b31f32ac7751e6ebce9371a16ca21b38
Jose Soto <jsoto7087@gmail.com>
2023-01-29 10:20:13 -0500

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+(1, "Mr. Chocolate", 101, "M", "Brown", "pretty Good", 1),
+(2, "Rowdy", 151, "M", "Orange", "needs work", 1),
+(3, "Tabitha", 10, "F", "Blue", "pretty Good", 1),
+(4, "Sergeant Brown", 301, "M", "Brown", "Angry all the time", 1),
+(5, "Melissa", 33, "F", "White", "Good", 1),
+(6, "Grinch", 55, "M", "Green", "Angry", 1),
+(7, "Whendy", 101, "M", "Green", "pretty Good", 1),
+(8, NULL, 777, "M", "PURPLE", "pretty Good", 1);
+
